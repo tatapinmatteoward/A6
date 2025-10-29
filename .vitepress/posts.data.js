9264b2f3876c6a22f301ab99c358bda4cde869a6
tatapinmatteoward <athcdejd81rul5@hotmail.com>
2025-10-29 11:28:02 +0000

    Add .vitepress/posts.data.js

---
+++ .vitepress/posts.data.js
@@ -0,0 +1,45 @@
+export default {
+  load() {
+    return [
+    {
+        "url": "/blog/calming-anxiety-naturally-with-valerian-root-and-lemon-balm-teas",
+        "title": "Calming Anxiety Naturally with Valerian Root and Lemon Balm Teas",
+        "excerpt": "Discover how Valerian Root and Lemon Balm teas can provide natural relief from anxiety, promoting relaxation without harsh side effects.",
+        "date": "2023-10-15",
+        "tags": [
+            "anxiety relief",
+            "natural remedies",
+            "herbal teas",
+            "valerian root",
+            "lemon balm"
+        ]
+    },
+    {
+        "url": "/blog/healing-minor-wounds-naturally-aloe-vera-manuka-honey",
+        "title": "Healing Minor Wounds Naturally: The Power of Aloe Vera and Manuka Honey",
+        "excerpt": "Discover how nature's remedies like aloe vera and manuka honey can accelerate the healing of minor wounds, reducing inflammation and preventing infection with simple home applications.",
+        "date": "2023-10-05",
+        "tags": [
+            "natural remedies",
+            "wound healing",
+            "aloe vera",
+            "manuka honey",
+            "home remedies"
+        ]
+    },
+    {
+        "url": "/blog/alleviating-joint-pain-with-turmeric-golden-milk-and-topical-compresses",
+        "title": "Alleviating Joint Pain Naturally: Turmeric Golden Milk and Topical Compresses",
+        "excerpt": "Discover effective natural remedies to ease joint pain using turmeric golden milk and soothing topical compresses. Learn recipes, benefits, and tips for inflammation relief at home.",
+        "date": "2023-10-05",
+        "tags": [
+            "joint pain",
+            "turmeric",
+            "natural remedies",
+            "golden milk",
+            "anti-inflammatory"
+        ]
+    }
+]
+  }
+}
